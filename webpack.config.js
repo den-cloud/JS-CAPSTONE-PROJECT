d1fd2c19a9367f1f25f7b52f70b9c1f954da6913
den-cloud <djpants90@yahoo.com>
2021-11-23 17:36:08 +0200

    Setup image loader

--- webpack.config.js
+++ webpack.config.js
@@ -18,9 +18,15 @@ module.exports = {
     module: {
         rules: [
             {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+            },
+            
+            {
                 test: /\.css$/i,
                 use: ['style-loader', 'css-loader'],
             },
         ],
     },
+
 };
