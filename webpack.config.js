a03c3ef90053587e7d9550660b7d92be2e0e9e60
den-cloud <djpants90@yahoo.com>
2021-11-23 17:27:09 +0200

    Setting up HtmlWebpackPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,18 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+    mode: 'development',
+
     entry: './src/index.js',
     output: {
         filename: 'main.js',
         path: path.resolve(__dirname, 'dist'),
+        clean: true,
     },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Output Management',
+        }),
+    ],
 };
